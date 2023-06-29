1c14b0c189b45e5a5690438ac3fc66bccaa0c903
rafael kasinski <rakasin@gmail.com>
2023-06-29 18:00:12 -0300

    Refactor handleCheckboxChange to use async/await and enhance logic for qualifiers sorting

--- src/components/QualifierList.tsx
+++ src/components/QualifierList.tsx
@@ -24,7 +24,7 @@ export default function QualifierList() {
       .catch(error => console.error('Error fetching data: ', error))
   }, [])
 
-  function handleCheckboxChange(qualifier: Qualifier, event: ChangeEvent<HTMLInputElement>) {
+  async function handleCheckboxChange(qualifier: Qualifier, event: ChangeEvent<HTMLInputElement>) {
     let newCheckedQualifiers = [...checkedQualifiers]
     if (event.target.checked) {
       newCheckedQualifiers.push(qualifier)
@@ -32,43 +32,25 @@ export default function QualifierList() {
       newCheckedQualifiers = newCheckedQualifiers.filter(q => q.id !== qualifier.id)
     }
 
-    axios.post('http://localhost:5000/api/calculateQualifierYears', { qualifiers: newCheckedQualifiers })
-      .then(response => {
-        setCheckedQualifiers(newCheckedQualifiers)
-        setTotalYears(response.data.totalYears)
-        setBaseSentence(response.data.baseSentence)
+    // Sort qualifiers based on their order in `qualifiers`
+    newCheckedQualifiers.sort((a, b) => {
+      return qualifiers.indexOf(a) - qualifiers.indexOf(b)
+    })
+
+    // Send the newCheckedQualifiers to the server
+    try {
+      const response = await axios.post('http://localhost:5000/api/calculateQualifierYears', {
+        "qualifiers": newCheckedQualifiers
       })
-      .catch(error => console.error('Error fetching data: ', error))
+
+      const data = response.data
+      setCheckedQualifiers(data.checkedQualifiers)
+      setTotalYears(data.totalYears)
+      setBaseSentence(data.baseSentence)
+    } catch (error) {
+      console.error('Error fetching data: ', error)
+    }
   }
-
-    // // Sort qualifiers based on their order in `qualifiers`
-    // newCheckedQualifiers.sort((a, b) => {
-    //   return qualifiers.indexOf(a) - qualifiers.indexOf(b)
-    // })
-
-  //   const additionalYears = (newCheckedQualifiers.length > 1) ? (newCheckedQualifiers.length - 1) * 1.5 : 0
-  //   newCheckedQualifiers.forEach((q, index) => {
-  //     if (index === 0) {
-  //       q.years = 0
-  //     } else {
-  //       q.years = 1.5
-  //     }
-  //   })
-
-  //   setCheckedQualifiers(newCheckedQualifiers)
-  //   setTotalYears(additionalYears)
-
-  //   let newBaseSentence = baseSentence
-  //   if (newCheckedQualifiers.length === 1) {
-  //     newBaseSentence = 12 // when the first qualifier is selected, the base sentence becomes 12 years
-  //   } else if (newCheckedQualifiers.length > 1) {
-  //     newBaseSentence = 12 + additionalYears // for each additional qualifier beyond the first one, add 1.5 years
-  //   } else {
-  //     newBaseSentence = 6 // if there are no qualifiers, the base sentence is 6 years
-  //   }
-  //   setBaseSentence(newBaseSentence)
-  // }
-
 
   return (
     <VStack
@@ -92,14 +74,14 @@ export default function QualifierList() {
                   {qualifier.label}
                 </Tooltip>
               </Checkbox>
-              {qualifier.years === 0
-              ? <Tooltip label="Apenas qualifica o crime">
-                  <Text visibility={checkedQualifiers.includes(qualifier) && checkedQualifiers.length > 1 ? "visible" : "hidden"}>
+              { checkedQualifiers[0]?.id === qualifier.id && checkedQualifiers.length > 1 ?
+                <Tooltip label="Apenas qualifica o crime">
+                  <Text visibility="visible">
                     {qualifier.years}
                   </Text>
-                </Tooltip>
-              : <Text visibility={checkedQualifiers.includes(qualifier) && checkedQualifiers.length > 1 ? "visible" : "hidden"}>
-                {qualifier.years}
+                </Tooltip> :
+                <Text visibility={checkedQualifiers.find(q => q.id === qualifier.id && q.years > 0) ? "visible" : "hidden"}>
+                  {checkedQualifiers.find(q => q.id === qualifier.id)?.years || 0}
                 </Text>
               }
             </HStack>
