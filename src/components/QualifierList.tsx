413657c264d3cdead26b961ac704819515ccd975
rafael kasinski <rakasin@gmail.com>
2023-06-28 21:59:56 -0300

    Add axios dependency and fetch qualifiers from API in QualifierList component

--- src/components/QualifierList.tsx
+++ src/components/QualifierList.tsx
@@ -1,20 +1,18 @@
 import { VStack, StackDivider, Tooltip, Box, HStack, Checkbox, Text, Flex, Spacer } from "@chakra-ui/react"
-import { useState } from "react"
-
-const qualifiers = [
-  { id: "qualifier-1", label: "inciso I", value: "mediante paga ou promessa de recompensa, ou por outro motivo torpe", years: 0 },
-  { id: "qualifier-2", label: "inciso II", value: "por motivo fútil", years: 0 },
-  { id: "qualifier-3", label: "inciso III", value: "com emprego de veneno, fogo, explosivo, asfixia, tortura ou outro meio insidioso ou cruel, ou de que possa resultar perigo comum", years: 0 },
-  { id: "qualifier-4", label: "inciso IV", value: "à traição, de emboscada, ou mediante dissimulação ou outro recurso que dificulte ou torne impossível a defesa do ofendido", years: 0 },
-  { id: "qualifier-5", label: "inciso V", value: "para assegurar a execução, a ocultação, a impunidade ou vantagem de outro crime", years: 0 },
-  { id: "qualifier-6", label: "inciso VI", value: "contra a mulher por razões da condição de sexo feminino", years: 0 },
-  { id: "qualifier-7", label: "inciso VII", value: "contra autoridade ou agente descrito nos arts. 142 e 144 da Constituição Federal, integrantes do sistema prisional e da Força Nacional de Segurança Pública, no exercício da função ou em decorrência dela, ou contra seu cônjuge, companheiro ou parente consanguíneo até terceiro grau, em razão dessa condição", years: 0 },
-  { id: "qualifier-8", label: "inciso VIII", value: "com emprego de arma de fogo de uso restrito ou proibido", years: 0 },
-  { id: "qualifier-9", label: "inciso IX", value: "contra menor de 14 (quatorze) anos", years: 0 },
-]
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export default function QualifierList() {
   const [totalYears, setTotalYears] = useState(0)
+  const [qualifiers, setQualifiers] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/qualifiers')
+      .then(response => {
+        setQualifiers(response.data)
+      })
+      .catch(error => console.error('Error fetching data: ', error))
+  }, [])
 
   function handleCheckboxChange(qualifier, event) {
     if (event.target.checked) {
