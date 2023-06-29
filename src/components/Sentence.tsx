66cbbfa4fd0716be24278d8b0d1a5f9667789d48
rafael kasinski <rakasin@gmail.com>
2023-06-29 14:03:09 -0300

    Update heading text in Sentence component

--- src/components/Sentence.tsx
+++ src/components/Sentence.tsx
@@ -4,7 +4,7 @@ import { Box, Heading } from "@chakra-ui/react"
 export default function Sentence() {
   return (
     <Box p={1} maxH="35rem">
-      <Heading as="h2" size="md" textAlign="center" pb={4}>Homicídio Qualificado</Heading>
+      <Heading as="h2" size="md" textAlign="center" pb={4}>Homicídio</Heading>
       <Heading as="h3" size="md">Clique nas qualificadoras do caso</Heading>
       <QualifierList />
     </Box>
